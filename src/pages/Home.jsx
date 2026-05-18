034912e88d3afdecdd6d634aff0fa685ad9460d7
kirillbozhko273 <kirillbozhko273@gmail.com>
2026-05-18 17:47:09 +1000

    seo home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 import Hero from '../components/Hero';
 import Devices from '../components/Devices';
 
 import Categories from '../components/Categories';
 
+
+
 function Home() {
   return (
+    <>
+  <Helmet>
+    <title>
+      TECHLIVE — Игровые ПК
+    </title>
+
+    <meta
+      name='description'
+      content='TECHLIVE — магазин игровых ПК, мониторов и периферии.'
+    />
+  </Helmet>
     <>
     <div className='page-animation'></div>
       <Hero />
       <Devices />
       <Categories />
+    </>
     </>
   );
 }

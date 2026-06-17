54ab74250907728878ec72a92012c295d9895b9e
kirillbozhko273 <kirillbozhko273@gmail.com>
2026-06-18 00:29:53 +1000

    dorabotka15

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,50 +1,128 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 
 import { FiSearch } from 'react-icons/fi';
 import { FiShoppingCart } from 'react-icons/fi';
 import { FiUser } from 'react-icons/fi';
+
 import { Link } from 'react-router-dom';
 
-
 function Header() {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <header className='header'>
-      <div className='logo'>
-        <h1>TECHLIVE</h1>
-      </div>
+    <>
 
-      <nav>
-        <Link to='/'>Главная</Link>
-        <Link to='/catalog'>Каталог</Link>
-        <Link to='/support'>Поддержка</Link>
-        <Link to='/community'>Сообщество</Link>
-        <Link to='/auth'></Link>
- 
-        
-      </nav>
+      <header className='header'>
 
-      <div className='header-icons'>
-        <button>
-          <FiSearch />
+        <div className='logo'>
+          <h1>TECHLIVE</h1>
+        </div>
+
+        <nav>
+          <Link to='/'>Главная</Link>
+          <Link to='/catalog'>Каталог</Link>
+          <Link to='/support'>Поддержка</Link>
+          <Link to='/community'>Сообщество</Link>
+        </nav>
+
+        <div className='header-icons'>
+
+          <button
+            className='burger-btn'
+            onClick={() => setMenuOpen(true)}
+          >
+            ☰
+          </button>
+
+          <button>
+            <FiSearch />
+          </button>
+
+          <Link
+            to='/cart'
+            className='cart-icon'
+          >
+            <FiShoppingCart />
+          </Link>
+
+          <Link
+            to='/auth'
+            className='login-btn'
+          >
+            <FiUser />
+            Вход
+          </Link>
+
+        </div>
+
+      </header>
+
+      {menuOpen && (
+        <div
+          className='mobile-overlay'
+          onClick={() => setMenuOpen(false)}
+        />
+      )}
+
+      <div
+        className={`mobile-menu ${
+          menuOpen ? 'mobile-menu-open' : ''
+        }`}
+      >
+
+        <button
+          className='close-menu'
+          onClick={() => setMenuOpen(false)}
+        >
+          ✕
         </button>
 
-  <Link
-  to='/cart'
-  className='cart-icon'
->
-  <FiShoppingCart />
-</Link>
         <Link
-  to='/auth'
-  className='login-btn'
->
-  <FiUser />
+          to='/'
+          onClick={() => setMenuOpen(false)}
+        >
+          Главная
+        </Link>
 
-  Вход
-</Link>
+        <Link
+          to='/catalog'
+          onClick={() => setMenuOpen(false)}
+        >
+          Каталог
+        </Link>
+
+        <Link
+          to='/support'
+          onClick={() => setMenuOpen(false)}
+        >
+          Поддержка
+        </Link>
+
+        <Link
+          to='/community'
+          onClick={() => setMenuOpen(false)}
+        >
+          Сообщество
+        </Link>
+
+        <Link
+          to='/cart'
+          onClick={() => setMenuOpen(false)}
+        >
+          Корзина
+        </Link>
+
+        <Link
+          to='/auth'
+          onClick={() => setMenuOpen(false)}
+        >
+          Вход
+        </Link>
+
       </div>
-    </header>
+
+    </>
   );
 }
 
